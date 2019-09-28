7414c68b8745b1fd9cd55ee88dd3549ff570be42
Christian Khawam <christiankhawam@outlook.com>
2019-09-27 20:11:19 -0400

    testing nightmareJs scraping library

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-const requestPromise = require('request-promise');
-const cheerio = require('cheerio');
-
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({ show: true });
 
 (async () => {
-    const USERNAME = 'instagram';
-    const BASE_URL = `https://instagram.com/${USERNAME}`
-
-    let response = await requestPromise(BASE_URL)
-    // console.log(response)
-
-    let $ = cheerio.load(response)
-
-    //Instagram uses graphql meaning, json is available to use in the front end.
-    //targeting the 4th script tag to grab the variable containing json object.
-    let script = $('script[type="text/javascript"]').eq(3).html();
-
-    //practicing regex to clean up the data then running it on the script variable.
-    let script_regex = /window._sharedData = (.+);/g.exec(script);
-
-    //parasing the data and practicing ES6 destructuring and get to the user.
-    let { entry_data: { ProfilePage: { [0]: { graphql: { user } } } } } = JSON.parse(script_regex[1]);
-
-    //setting the data to an object with the information I want.
-    let instagram_data = {
-        fullName: user.full_name,
-        profilePic: user.profile_pic_url_hd,
-        followers: user.edge_followed_by.count,
-        following: user.edge_follow.count,
-        posts: user.edge_owner_to_timeline_media.count
+    try {
+        await nightmare.goto('https://duckduckgo.com');
+        await nightmare.type('#search_form_input_homepage', 'github nightmare');
+        await nightmare.click('#search_button_homepage');
+        await nightmare.wait('#r1-0 a.result__a');
+        let link = await nightmare.evaluate(() => document.querySelector('#r1-0 a.result__a').href)
+        await nightmare.end()
+        console.log(link)
+    } catch (error) {
+        console.log(`Something happened: ${error}`)
     }
-
-
-    console.log(instagram_data)
 })()
---
+++ instagram/index.js
@@ -0,0 +1,35 @@
+const requestPromise = require('request-promise');
+const cheerio = require('cheerio');
+
+
+(async () => {
+    const USERNAME = 'instagram';
+    const BASE_URL = `https://instagram.com/${USERNAME}`
+
+    let response = await requestPromise(BASE_URL)
+    // console.log(response)
+
+    let $ = cheerio.load(response)
+
+    //Instagram uses graphql meaning, json is available to use in the front end.
+    //targeting the 4th script tag to grab the variable containing json object.
+    let script = $('script[type="text/javascript"]').eq(3).html();
+
+    //practicing regex to clean up the data then running it on the script variable.
+    let script_regex = /window._sharedData = (.+);/g.exec(script);
+
+    //parsing the data and practicing ES6 destructuring and get to the user.
+    let { entry_data: { ProfilePage: { [0]: { graphql: { user } } } } } = JSON.parse(script_regex[1]);
+
+    //setting the data to an object with the information I want.
+    let instagram_data = {
+        fullName: user.full_name,
+        profilePic: user.profile_pic_url_hd,
+        followers: user.edge_followed_by.count,
+        following: user.edge_follow.count,
+        posts: user.edge_owner_to_timeline_media.count
+    }
+
+
+    console.log(instagram_data)
+})()
